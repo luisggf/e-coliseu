cc84e08085b8501de021d9e8314351447693b5fd
luisggf <luis.guigfonseca@gmail.com>
2023-08-14 14:31:04 -0300

    final ver

--- api/server/infra/database.js
+++ api/server/infra/database.js
@@ -1,10 +1,10 @@
 const pgp = require("pg-promise")();
 const db = pgp({
   user: "postgres",
-  password: "8546",
+  password: "1234",
   host: "localhost",
   port: 5432,
-  database: "tp_bd",
+  database: "postgres",
 });
 
 module.exports = db;
